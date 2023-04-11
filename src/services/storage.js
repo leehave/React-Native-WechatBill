02304a56eeaaa86a2a8e5a798c12ebc36c58be48
leehave <1831553725@qq.com>
2023-04-11 21:03:24 +0800

    feat(dev): 类型转换

--- src/services/storage.js
+++ src/services/storage.js
@@ -7,21 +7,21 @@
 import AsyncStorage from '@react-native-community/async-storage'
 import { STORAGE } from '@app/constants/storage'
 
-export const get = <T>(key: STORAGE): Promise<T> => {
+export const get = (key) => {
   return AsyncStorage.getItem(key).then(data => {
     return data ? JSON.parse(data) : data
   })
 }
 
-export const set = (key: STORAGE, value: any): Promise<void> => {
+export const set = (key, value)=> {
   return AsyncStorage.setItem(key, JSON.stringify(value))
 }
 
-export const remove = (key: STORAGE): Promise<void> => {
+export const remove = (key)=> {
   return AsyncStorage.removeItem(key)
 }
 
-export const clear = (): Promise<void> => {
+export const clear = ()=> {
   return AsyncStorage.clear()
 }
 
